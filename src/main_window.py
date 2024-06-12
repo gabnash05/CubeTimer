3600c832f41e7da83dc2ff8b2c1ccf984d2920ce
kimgab <nasayaokim@gmail.com>
2024-06-12 20:17:54 +0800

    fixed link problem when loading ui

--- src/main_window.py
+++ src/main_window.py
@@ -1,12 +1,15 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon
+import os
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi('cubeTimer.ui', self)
+
+        uiPath = os.path.join(os.path.dirname(__file__), "E:/Programming/Programming Practice/CubeTimer/src/ui/cubeTimer.ui")
+        uic.loadUi(uiPath, self)
 
 
 
