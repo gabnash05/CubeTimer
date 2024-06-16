088a641be925142a7a843faa371a39879b9aa38a
kimgab <nasayaokim@gmail.com>
2024-06-16 22:30:28 +0800

    minor sqll query bug fix

--- src/models/databaseModel.py
+++ src/models/databaseModel.py
@@ -10,17 +10,17 @@ class DatabaseModel():
   def _create_table(self):
     query = """
           CREATE TABLE IF NOT EXISTS three_cube_times (
-          id INT AUTO_INCREMENT PRIMARY KEY,
-              user_id INT NOT NULL,
+          id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
           solve_time FLOAT NOT NULL,
           scramble VARCHAR(255) NOT NULL,
           solve_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-          is_plus_2 BOOL NOT NULL,
-          is_DNF BOOL NOT NULL
+          is_plus_2 BOOL NOT NULL DEFAULT FALSE,
+          is_DNF BOOL NOT NULL DEFAULT FALSE
           ); """
     
     self.cursor.execute(query)
     self.connection.commit()
+
 
   def saveTimeRecord(self, solve_time, scramble):
     query = "INSERT INTO three_cube_times (solve_time, scramble) VALUES (%s, %s)"
@@ -28,4 +28,26 @@ class DatabaseModel():
 
     self.cursor.execute(query, values)
     self.connection.commit()
+
+
+  def getTimeRecords(self):
+    query = "SELECT * FROM three_cube_times ORDER BY id LIMIT 50"
+
+    self.cursor.execute(query)
+    self.connection.commit()
+    result = self.cursor.fetchall()
+
+    return result
+
+
+  def getTimeRecords(self, page):
+    if page > 0:
+      query = f"SELECT * FROM three_cube_times ORDER BY id LIMIT 50 OFFSET {(page - 1) * 50}"
+
+      self.cursor.execute(query)
+      self.connection.commit()
+      result = self.cursor.fetchall()
+
+      return result
+  
   
